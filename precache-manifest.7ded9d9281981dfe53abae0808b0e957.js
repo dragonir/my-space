c9bcce29632c1bae113c8587eac5002d65b0516b
dragonir <dragonirxo@outlook.com>
2019-01-28 09:01:43 +0800

    Updates

---
+++ precache-manifest.7ded9d9281981dfe53abae0808b0e957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-space/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "863b6279d210e81f1c23",
+    "url": "/my-space/static/js/runtime~main.863b6279.js"
+  },
+  {
+    "revision": "83107bc2904ad8e5e665",
+    "url": "/my-space/static/js/main.83107bc2.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/my-space/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "83107bc2904ad8e5e665",
+    "url": "/my-space/static/css/main.9cfcb700.chunk.css"
+  },
+  {
+    "revision": "9819a61084d92d891921f6d0da5a93d8",
+    "url": "/my-space/index.html"
+  }
+];

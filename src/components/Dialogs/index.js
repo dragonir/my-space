4f64a58ecbc3bbbb7d6d24abc8d1fb235aa189cf
dragonir <dragonirxo@outlook.com>
2019-02-25 08:58:05 +0800

    Feat: add SimpleDialog component

--- src/components/Dialogs/index.js
+++ src/components/Dialogs/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import SimpleDialogDemo from './SimpleDialog';
 
 function Dialogs(){
   return (
     <div>
       <h1>Dialogs</h1>
+      <h2>基本对话框 SimpleDialog</h2>
+      <SimpleDialogDemo />
     </div>
   )
 }

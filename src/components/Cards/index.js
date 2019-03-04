6bc1ffe1bdd4f7b762089cab79ac4391b1761d6e
dragonir <dragonirxo@outlook.com>
2019-03-04 08:52:44 +0800

    feat: add Card route and SimpleCard component.

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import SimpleCard from './SimpleCard';
 
 function Cards(){
   return (
     <div>
       <h1>Cards 卡片</h1>
+      <h2>SimpleCard 简单卡片</h2>
+      <SimpleCard />
     </div>
   )
 }

d8dec925a7b0dc9bf944bdbf90bad7d8456e0c73
scofiled <dragonirxo@outlook.com>
2019-03-10 22:38:57 +0800

    Feat: add Index component

--- src/containers/Work/index.js
+++ src/containers/Work/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const About = () => {
+const Work = () => {
     return (
         <div>
-            <h1>About</h1>
+            <h1>Work</h1>
         </div>
     )
 }
 
-export default About;
+export default Work;

fe50dbb2abf85b5e07848ea41a9c41e26d87adee
scofiled <dragonirxo@outlook.com>
2019-05-17 01:07:34 +0800

    Updates

---
+++ precache-manifest.dddc26235a44621e00e9f736a8f13cbe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f40a9d53a5eac134f217f676f44eddab",
+    "url": "/static/media/drib_2x.f40a9d53.jpg"
+  },
+  {
+    "revision": "1801adce3aed50ac26c7",
+    "url": "/static/css/main.e2c4e1dc.chunk.css"
+  },
+  {
+    "revision": "b6c4e8db4beb56ff4b30",
+    "url": "/static/js/1.b6c4e8db.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a388958ec50eff81cf5842fc1aaf3c42",
+    "url": "/static/media/team_final.a388958e.jpg"
+  },
+  {
+    "revision": "e6b276ae2ea4e962fd3717fb5d8ae187",
+    "url": "/static/media/ruby2.e6b276ae.png"
+  },
+  {
+    "revision": "1801adce3aed50ac26c7",
+    "url": "/static/js/main.1801adce.chunk.js"
+  },
+  {
+    "revision": "6c534e5ec8787cc2a50a6bc9f9119a84",
+    "url": "/static/media/pink_cloud.6c534e5e.png"
+  },
+  {
+    "revision": "a1f784fb7a46a13b90164457b0773f4d",
+    "url": "/static/media/bart.a1f784fb.jpg"
+  },
+  {
+    "revision": "d90af582a325fa7db86124423888c4d2",
+    "url": "/static/media/home.d90af582.png"
+  },
+  {
+    "revision": "e6b276ae2ea4e962fd3717fb5d8ae187",
+    "url": "/static/media/logo.e6b276ae.png"
+  },
+  {
+    "revision": "50f32ef6f0bbf82de00a706852f810db",
+    "url": "/static/media/paella.50f32ef6.jpg"
+  },
+  {
+    "revision": "910b1ca7cc8a2e9aae641ce3794f7f43",
+    "url": "/index.html"
+  }
+];

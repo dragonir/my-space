5be4cfdb8e5cf1c2c33b1cb5e423d6498d2dbb7c
scofiled <dragonirxo@outlook.com>
2020-09-15 23:11:23 +0800

    feat: about页面样式优化.

--- src/containers/Material/index.js
+++ src/containers/Material/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Thirty from '../../components/30s/index.js';
 
 const Material = (props) => {
   return (
     <div>
       <h1>Material</h1>
+      <Thirty />
     </div>
   );
 }

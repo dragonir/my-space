e5fb089eef4d0ecf40fb7984e68a75f17da18032
scofiled <dragonirxo@outlook.com>
2020-09-15 23:28:45 +0800

    Updates

---
+++ precache-manifest.41d234b2b364c4b8300322a1ffbf2501.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717df6dde6d462e4357040e09cdd85ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3829ce8b1c26d1d4105",
+    "url": "/static/css/2.9bb1a5e2.chunk.css"
+  },
+  {
+    "revision": "d6af44e5997703a3bf3a",
+    "url": "/static/css/main.071430f3.chunk.css"
+  },
+  {
+    "revision": "f3829ce8b1c26d1d4105",
+    "url": "/static/js/2.e993bb17.chunk.js"
+  },
+  {
+    "revision": "d22654696111c6da8944f86bd3df14cd",
+    "url": "/static/js/2.e993bb17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6af44e5997703a3bf3a",
+    "url": "/static/js/main.52e707b2.chunk.js"
+  },
+  {
+    "revision": "13256f46ead4335b55dc",
+    "url": "/static/js/runtime-main.0f34b1b0.js"
+  },
+  {
+    "revision": "5dfa8de43a74df062245d83caf41606b",
+    "url": "/static/media/404.5dfa8de4.svg"
+  },
+  {
+    "revision": "63f874d192fb3892d88d5e26f942b5e2",
+    "url": "/static/media/DS-DIGI.63f874d1.TTF"
+  },
+  {
+    "revision": "1243ed3466eba463ea8b1e2f627ce35c",
+    "url": "/static/media/air.1243ed34.ttf"
+  },
+  {
+    "revision": "a1f784fb7a46a13b90164457b0773f4d",
+    "url": "/static/media/bart.a1f784fb.jpg"
+  },
+  {
+    "revision": "01d4676b83d27b40346ec476758d64c5",
+    "url": "/static/media/bottom_bg.01d4676b.png"
+  },
+  {
+    "revision": "cf148b992a09d2b58c83f63db93068da",
+    "url": "/static/media/contact.cf148b99.png"
+  },
+  {
+    "revision": "3bd0bac4cdb843a6f1d7a207d20872b0",
+    "url": "/static/media/drib_2x.3bd0bac4.jpg"
+  },
+  {
+    "revision": "861fa9156342e913878b6b327205a0ba",
+    "url": "/static/media/empty.861fa915.jpg"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/static/media/icons-20.cef8cdbb.woff"
+  },
+  {
+    "revision": "5eaab751894f443f7e5bfea569e15c07",
+    "url": "/static/media/logo.5eaab751.png"
+  },
+  {
+    "revision": "34dfeed20d644ba572bd2d8d31bc8d77",
+    "url": "/static/media/mediaCard.34dfeed2.jpg"
+  },
+  {
+    "revision": "50f32ef6f0bbf82de00a706852f810db",
+    "url": "/static/media/paella.50f32ef6.jpg"
+  },
+  {
+    "revision": "73cf22bfc7e7d1bb9f0a23d3d3b96645",
+    "url": "/static/media/pink_cloud.73cf22bf.png"
+  },
+  {
+    "revision": "db8d832ae822ca5ddf486c8a7211ee93",
+    "url": "/static/media/put-down.db8d832a.svg"
+  },
+  {
+    "revision": "e6b276ae2ea4e962fd3717fb5d8ae187",
+    "url": "/static/media/ruby2.e6b276ae.png"
+  },
+  {
+    "revision": "a388958ec50eff81cf5842fc1aaf3c42",
+    "url": "/static/media/team_final.a388958e.jpg"
+  }
+]);
